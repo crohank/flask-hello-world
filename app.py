0276ce37beb1e0a226bb9deaf028d1e5a7f9d566
Rohan Kumar <crohank02@gmail.com>
2023-03-16 14:58:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,request,jsonify
+import numpy as np
+import pickle
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+flask_app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+
+
+@flask_app.route("/")
+def index():
+    return "Hello world"
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    cgpa = request.form.get('cgpa')
+    iq = request.form.get('iq')
+    profile_score = request.form.get('profile_score')
+
+    input_query = np.array([[cgpa,iq,profile_score]])
+
+    result = model.predict(input_query)[0]
+
+    return jsonify({'placement':str(result)})
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
